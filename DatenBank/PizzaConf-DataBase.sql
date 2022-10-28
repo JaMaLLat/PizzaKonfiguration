957ddb4c36d2f670fe0b52ee6404a848f50e3593
Jamal <Jamallata333@gmail.com>
2022-10-28 03:54:28 +0200

    FremdSclhüssel Anliegen

--- DatenBank/PizzaConf-DataBase.sql
+++ DatenBank/PizzaConf-DataBase.sql
@@ -8,14 +8,18 @@ USE DbPizzaconf
 GO
 
 
-IF OBJECT_ID('Pizza') IS NULL 
+IF OBJECT_ID('Pizza') IS not NULL 
+DROP TABLE Pizza 
 CREATE TABLE Pizza 
 (
 PizzaID INT NOT NULL IDENTITY(1,1) ,
+RechnungID INT ,
 Groﬂe FLOAT NOT NULL ,
 Beschreibung VARCHAR(2000) NOT NULL ,
 MENGE INT NOT NULL ,
 PRIMARY KEY (PizzaID),
+FOREIGN KEY (RechnungID) REFERENCES Rechnung(RechnungID)
+
 )
 GO
 IF OBJECT_ID('Zutaten') IS NOT NULL
@@ -23,10 +27,14 @@ DROP TABLE Zutaten
 CREATE TABLE Zutaten
 (
  ZutatenID INT NOT NULL IDENTITY(1,1),
+ PizzaID INT ,
+ ZutatenArtID INT,
  Sort VARCHAR(50) NOT NULL ,
  Menge INT ,
  Preis MONEY ,
  PRIMARY KEY (ZutatenID),
+ FOREIGN KEY (PizzaID) REFERENCES Pizza(PizzaID),
+ FOREIGN KEY (ZutatenArtID) REFERENCES ZutatenArt(ZutatenArtID)
 )
 GO
 
@@ -58,8 +66,11 @@ DROP TABLE Teig
 CREATE TABLE Teig
 (
  Sort VARCHAR(50) NOT NULL ,
+ ZutatenArtID INT,
  Preis MONEY ,
  PRIMARY KEY (Sort),
+ FOREIGN KEY (ZutatenArtID) REFERENCES ZutatenArt(ZutatenArtID)
+
 )
 
 INSERT INTO Teig ( Sort , Preis) VALUES
@@ -72,8 +83,11 @@ DROP TABLE Pizzasoﬂe
 CREATE TABLE Pizzasoﬂe
 (
  Sort VARCHAR(50) NOT NULL ,
+ ZutatenArtID INT,
  Preis MONEY ,
  PRIMARY KEY (Sort),
+ FOREIGN KEY (ZutatenArtID) REFERENCES ZutatenArt(ZutatenArtID)
+
 )
 
 INSERT INTO Pizzasoﬂe ( Sort , Preis) VALUES
@@ -87,8 +101,11 @@ DROP TABLE Fleisch
 CREATE TABLE Fleisch
 (
  Sort VARCHAR(50) NOT NULL ,
+  ZutatenArtID INT,
  Preis MONEY ,
  PRIMARY KEY (Sort),
+ FOREIGN KEY (ZutatenArtID) REFERENCES ZutatenArt(ZutatenArtID)
+
 )
 
 INSERT INTO Fleisch ( Sort , Preis) VALUES
@@ -103,8 +120,12 @@ DROP TABLE Vegetarische
 CREATE TABLE Vegetarische
 (
  Sort VARCHAR(50) NOT NULL ,
+ ZutatenArtID INT,
+
  Preis MONEY ,
  PRIMARY KEY (Sort),
+  FOREIGN KEY (ZutatenArtID) REFERENCES ZutatenArt(ZutatenArtID)
+
 )
 INSERT INTO Vegetarische ( Sort , Preis) VALUES
 ('Ananas' , 0.15) ,
@@ -121,13 +142,17 @@ DROP TABLE Fisch
 CREATE TABLE Fisch 
 (
  Sort VARCHAR(50) NOT NULL ,
+ ZutatenArtID INT,
  Preis MONEY ,
- PRIMARY KEY (Sort)
+ PRIMARY KEY (Sort),
+ FOREIGN KEY (ZutatenArtID) REFERENCES ZutatenArt(ZutatenArtID)
+
 )
 go
 INSERT INTO Fisch (Sort, Preis) VALUES
 ('Thunfisch', 0.40) ,
 ('Sardellen', 0.20) 
 GO
-select * from Fisch
+select * from Pizza
+
 
