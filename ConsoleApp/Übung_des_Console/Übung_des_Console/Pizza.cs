423d9bf8f6a17884f2d769cf374ff79e2b9ff5f5
Jamal <Jamallata333@gmail.com>
2022-11-17 18:57:02 +0100

    Erste möglichkeit aber nicht ideal

---
+++ ConsoleApp/Übung_des_Console/Übung_des_Console/Pizza.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Übung_des_Console
+{
+    [Serializable]
+    class Pizza
+    {
+        public int PizzaId;
+        public string Pizzaname;
+        public decimal Price;
+        
+
+        public Pizza( int pizzaid , string pizzaname , decimal price)
+        {
+            PizzaId = pizzaid;
+            Pizzaname = pizzaname;
+            Price = price;
+        }
+        
+    }
+    
+}
---
+++ ConsoleApp/Übung_des_Console/Übung_des_Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Übung_des_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string datei = @"C:\Schule\Klassen Projekt1\ConsoleApp\Übung_des_Console.scv";
+            Pizza ss = new Pizza(1, "Salami", 17.25m);
+            //using (var schreib = new StreamWriter(datei) )
+            //{
+               File.AppendAllText(datei, $"{ss.PizzaId.ToString()} + {ss.Pizzaname } + {ss.Price.ToString()}");
+
+            using (FileStream stream = new FileStream(datei, FileMode.Open))
+                
+
+
+                //}
+                Console.Read();
+          
+        }
+    }
+}
