c48db04b9bc5e2270b385abc9c40931fc968e726
Jamal <Jamallata333@gmail.com>
2022-11-17 19:16:12 +0100

    zweite möglichkeit read.write CSV.datei

--- ConsoleApp/Übung_des_Console/Übung_des_Console/Program.cs
+++ ConsoleApp/Übung_des_Console/Übung_des_Console/Program.cs
@@ -11,14 +11,24 @@ namespace Übung_des_Console
     {
         static void Main(string[] args)
         {
-            string datei = @"C:\Schule\Klassen Projekt1\ConsoleApp\Übung_des_Console.scv";
+            string datei = @"C:\Schule\Klassen Projekt1\ConsoleApp\Übung_des_Console1.csv";
             Pizza ss = new Pizza(1, "Salami", 17.25m);
             //using (var schreib = new StreamWriter(datei) )
             //{
-               File.AppendAllText(datei, $"{ss.PizzaId.ToString()} + {ss.Pizzaname } + {ss.Price.ToString()}");
+               //File.AppendAllText(datei, $"{ss.PizzaId.ToString()} + {ss.Pizzaname } + {ss.Price.ToString()}");
 
-            using (FileStream stream = new FileStream(datei, FileMode.Open))
-                
+
+            using (StreamWriter schr = new StreamWriter(new FileStream(datei, FileMode.Create, FileAccess.ReadWrite)))
+            {
+                schr.WriteLine("pizzaID" + " pizzaname" + " Price");
+                schr.WriteLine($"{ss.PizzaId} {ss.Pizzaname} {ss.Price}");
+            }
+
+            using(StreamReader stream = new StreamReader(datei))
+            {
+               string data =  File.ReadAllText(datei);
+                Console.WriteLine(data);
+            }
 
 
                 //}
